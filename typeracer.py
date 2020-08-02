3a92f91fbd5b2e409c036ec6c1dfb7b16cf37b34
Jun Choi <jun.s.choi@gmail.com>
2020-08-02 18:26:26 -0400

    commit

--- typeracer.py
+++ typeracer.py
@@ -24,7 +24,7 @@ class TypeRacer:
         # options.add_argument('--log-level=3')
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         # Set path as your ChromeDriver path below
-        path = r'\driver\chromedriver.exe'
+        path = r'driver\chromedriver.exe'
         self.driver = webdriver.Chrome(executable_path=path, options = options)
         self.menu()
 
